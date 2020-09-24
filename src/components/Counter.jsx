d24aaf591cb27ca1888b5508ac1d82c3a755e3db
jongmok1031 <jongmok1031@gmail.com>
2020-09-24 14:52:23 +0900

    end of reactjs

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,18 +2,21 @@ import React, {useState} from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
-    const handleClick = () => {
+    const handleClick_add = () => {
         setCount(count + 1)
     }
-    const handleClick2 = () => {
+    const handleClick2_subtract = () => {
         setCount(count - 1)
     }
     return <>
         <h1>Counter : {count}</h1>
-        <button onClick={handleClick}> + </button>
-        <button onClick={handleClick2}> subtract </button>
+        <button onClick={handleClick_add}> add one </button>
+        <button onClick={handleClick2_subtract}> subtract one </button>
     </>
 }
 export default Counter
 
-     
+    //  let 변수 const 상수
+    //  count getter 
+    //  기능은 상수로 써야돼
+    //  setCount setter
